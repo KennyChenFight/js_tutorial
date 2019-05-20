2c6700572f71c865651fae7a857158c4b2b14b87
KennyChen <kennychen851228@gmail.com>
2019-05-20 13:50:36 +0800

    math module demo

---
+++ ES6/module.mjs
@@ -0,0 +1,13 @@
+// import {max, sum, PI} from './math.mjs';
+
+// console.log(max(10, 5));
+// console.log(sum(1, 2, 3, 4, 5));
+// console.log(PI);
+
+// import {max as maximum} from './math.mjs';
+// console.log(maximum(10, 5));
+
+import * as math from './math.mjs';
+console.log(math.max(10, 5));
+console.log(math.sum(1, 2, 3, 4, 5));
+console.log(math.PI);
